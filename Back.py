91c47ae35f93c99199425ccc9b9f6d01d24b941a
amoghakoulapure <amoghakoulapure@gmail.com>
2024-09-20 16:31:47 +0530

    Update Back.py

--- Back.py
+++ Back.py
@@ -1,13 +1,1 @@
-# Import the necessary modules
-import scrapy
 
-# Define the Spider class
-class MySpider(scrapy.Spider):
-    # Name of the spider
-    name = "my_spider"
-
-    # Start URL
-    start_urls = [
-        'https://
-                'title': title,
-            }
