f678a93d58d5b19d905df325709d57cd54a3e5b0
anoynomous00 <amithbanakar7760@gmail.com>
2024-09-20 16:33:31 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,13 +1,13 @@
 # Function to add two numbers
-def add_numbers(num1, num2):
-    return num1 + num2
+def amul_numbers(num1, num2):
+    return num1 * num2
 
 # Input from the user
 number1 = float(input("Enter the first number: "))
 number2 = float(input("Enter the second number: "))
 
 # Adding the numbers
-result = add_numbers(number1, number2)
+result = mul_numbers(number1, number2)
 
 # Displaying the result
 print(f"The sum of {number1} and {number2} is {result}.")
