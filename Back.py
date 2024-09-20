98ba2c09d5e70dca8868cefc0eb5c355affe9b8b
amoghakoulapure <amoghakoulapure@gmail.com>
2024-09-20 16:30:20 +0530

    Update Back.py

--- Back.py
+++ Back.py
@@ -8,22 +8,6 @@ class MySpider(scrapy.Spider):
 
     # Start URL
     start_urls = [
-        'https://krama.karnataka.gov.in/reports/state.aspx?',
-    ]
-
-    # Define the parse function
-    def parse(self, response):
-        # Extract the title of the webpage
-        title = response.xpath('//title/text()').get()
-
-        # Handle errors
-        if title is None:
-            print('Error: Unable to extract title')
-        else:
-            # Print the title
-            print('Title:', title)
-
-            # Yield the title
-            yield {
+        'https://
                 'title': title,
             }
